16b23d3e7741883ec882db941a7ce0bf27125d7c
Pham Son <swlpham@gmail.com>
2021-11-30 00:03:39 +0700

    fix data config for etl.

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -40,9 +40,9 @@ DATASETS = {
 	},
 	'etl_test': {
 		'transforms': transforms_config.FontToKanjiTransform,
-		'train_source_root': dataset_paths['celeba_train'],
-		'train_target_root': dataset_paths['celeba_train'],
-		'test_source_root': dataset_paths['celeba_test'],
-		'test_target_root': dataset_paths['celeba_test'],
+		'train_source_root': dataset_paths['etl_train'],
+		'train_target_root': dataset_paths['etl_train'],
+		'test_source_root': dataset_paths['etl_test'],
+		'test_target_root': dataset_paths['etl_test'],
 	}
 }
